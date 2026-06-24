26f034222d958014435470721b1ddc53404099cd
Daniel Cooper <daniel.cooper@tantosec.com>
2026-06-24 14:23:20 +1000

    Updated nix deps

--- python-env.nix
+++ python-env.nix
@@ -3,8 +3,9 @@
   python3 ? pkgs.python3,
 }:
 python3.withPackages (ps: [
-  ps.requests
-  ps.pysocks # SOCKS5 proxy support (requests[socks])
+  ps.httpx
+  ps.h2 # HTTP/2 support (httpx[http2])
+  ps.socksio # SOCKS5 proxy support (httpx[socks])
   ps.brotli
   ps.zstandard
 ])
